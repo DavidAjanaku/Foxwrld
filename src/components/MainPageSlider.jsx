3d81bac0e7551fb89831b6b87565ff1007a366b0
DavidAjanaku <davidajanaku46163@gmail.com>
2023-07-01 12:27:43 +0100

    David

--- src/components/MainPageSlider.jsx
+++ src/components/MainPageSlider.jsx
@@ -57,13 +57,19 @@ class MainPageSlider extends React.Component {
     );
   }
 
+  handleVideoEnded = () => {
+    this.setState((prevState) => ({
+      currentSlide: (prevState.currentSlide + 1) % prevState.videos.length,
+    }));
+  };
+
   render() {
     const { videos, currentSlide } = this.state;
     const currentVideo = videos[currentSlide];
 
     return (
       <div className="main-page-slider w-full relative left-0 right-0 top-0">
-        <video className="w-full relative" src={currentVideo.videoUrl} autoPlay loop muted controls={false}>
+        <video className="w-full relative" src={currentVideo.videoUrl} autoPlay loop muted controls={false} onEnded={this.handleVideoEnded}>
           Your browser does not support the video tag.
         </video>
         <div className="text-overlay absolute bottom-12 left-0 right-0 text-center text-white">
