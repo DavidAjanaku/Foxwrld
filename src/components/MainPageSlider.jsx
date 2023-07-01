b9057cd635923d89a3a26fb802471b60d9e05ecc
DavidAjanaku <davidajanaku46163@gmail.com>
2023-07-01 11:30:32 +0100

    David

--- src/components/MainPageSlider.jsx
+++ src/components/MainPageSlider.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import LazyLoad from 'react-lazy-load';
+import { Link } from 'react-router-dom';
+
 import video1 from '../assets/videos/video1.mp4';
 import video2 from '../assets/videos/video2.mp4';
-import { Link } from 'react-router-dom';
 
 class MainPageSlider extends React.Component {
   constructor(props) {
@@ -18,9 +20,7 @@ class MainPageSlider extends React.Component {
           title: 'The Holiday Edit',
           videoUrl: video2,
           text: 'Men',
-
           text1: 'Women',
-
         },
       ],
       currentSlide: 0,
@@ -53,16 +53,21 @@ class MainPageSlider extends React.Component {
 
     return (
       <div className="main-page-slider w-full relative left-0 right-0 top-0">
-        <video className='w-full relative ' src={currentVideo.videoUrl} autoPlay loop muted>
-          Your browser does not support the video tag.
-        </video>
+        <LazyLoad>
+          <video className="w-full relative" src={currentVideo.videoUrl} autoPlay loop muted>
+            Your browser does not support the video tag.
+          </video>
+        </LazyLoad>
         <div className="text-overlay absolute bottom-12 left-0 right-0 text-center text-white">
-          <h2 className='text-white'>{currentVideo.title}</h2>
-         <div className='flex justify-center '>
-         <Link to="/" className='underline underline-offset-8'><p className='text-white m-2'>{currentVideo.text}</p></Link>
-          <Link to="/" className='underline underline-offset-8'><p className='text-white m-2'>{currentVideo.text1}</p></Link>
-         </div>
-
+          <h2 className="text-white">{currentVideo.title}</h2>
+          <div className="flex justify-center">
+            <Link to="/" className="underline underline-offset-8">
+              <p className="text-white m-2">{currentVideo.text}</p>
+            </Link>
+            <Link to="/" className="underline underline-offset-8">
+              <p className="text-white m-2">{currentVideo.text1}</p>
+            </Link>
+          </div>
         </div>
         <div className="gradient-overlay absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50"></div>
       </div>
