4f193734ced19b1c1a74569b29be7b8a50011c44
DavidAjanaku <davidajanaku46163@gmail.com>
2023-07-04 11:43:37 +0100

    working on it

--- src/components/MainPageSlider.jsx
+++ src/components/MainPageSlider.jsx
@@ -12,14 +12,15 @@ export default function MainPageSlider() {
     <div className="main relative">
       <div className="overlay absolute inset-0 bg-black opacity-50"></div>{" "}
       <video
-        ref={videoRef}
-        src={video2}
+        className="video-bg object-cover h-[70vh] min-w-full"
         autoPlay
         muted
         loop
         playsInline
-        className="object-cover h-[70vh] min-w-full"
-      />
+        ref={videoRef}
+      >
+        <source src={video2} type="video/webm" />
+      </video>
       <div className="absolute w-full h-full top-0 flex flex-col justify-center items-center  text-white">
         <h1>Welcome to</h1>
         <p className="text-4xl font-extrabold font-sans"> FOXWRLD</p>
