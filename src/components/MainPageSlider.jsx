71a541bbf70b68b3a7bd960425d2dd66082c5d0a
DavidAjanaku <davidajanaku46163@gmail.com>
2023-07-04 03:58:39 +0100

    David

--- src/components/MainPageSlider.jsx
+++ src/components/MainPageSlider.jsx
@@ -4,13 +4,13 @@ import video2 from '../assets/videos/video2.mp4';
 
 export default function MainPageSlider() {
   return (
-    <div className="video-container">
-     <video src={video2} autoPlay loop muted />
-      <div className="content">
-        {/* Place your content (e.g., text, buttons) on top of the video */}
-        <h1>Welcome to the website</h1>
-        <p>Some description about the page</p>
-      </div>
+    <div className='main'>
+    <div className="overlay"></div>
+    <video src={video2} autoPlay loop muted />
+    <div className="content">
+        <h1>Welcome</h1>
+        <p>To my site.</p>
     </div>
+</div>
   );
 }
