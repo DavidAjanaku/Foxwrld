81f93ac80a51724b43a86457621f7c1f2e71f1cb
DavidAjanaku <davidajanaku46163@gmail.com>
2023-07-04 04:04:04 +0100

    David

--- src/components/MainPageSlider.jsx
+++ src/components/MainPageSlider.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import video2 from '../assets/videos/video2.mp4';
 
-
 export default function MainPageSlider() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    videoRef.current.play();
+  }, []);
+
   return (
     <div className='main'>
-    <div className="overlay"></div>
-    <video src={video2} autoPlay loop  />
-    <div className="content">
+      <div className="overlay"></div>
+      <video ref={videoRef} src={video2} autoPlay muted loop playsInline />
+      <div className="content">
         <h1>Welcome</h1>
         <p>To my site.</p>
+      </div>
     </div>
-</div>
   );
 }
