90fd64d667a817c125991c7fabcd54ca9c855eab
DavidAjanaku <davidajanaku46163@gmail.com>
2023-07-12 14:35:44 +0100

    Done

--- src/views/Checkout.jsx
+++ src/views/Checkout.jsx
@@ -1,40 +1,42 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import Hamburger from '../components/Navigation/Hamburger';
 import Header from '../components/Navigation/Header';
-import { Link } from 'react-router-dom';
 import sandals from '../assets/sandals.jpeg';
 import CheckoutCard from '../components/Checkout/CheckoutCard';
 import Footer from '../components/Navigation/Footer';
 
 export default function Checkout() {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1); // Navigates back to the previous page
+  };
+
   return (
     <div>
-      <div className='h-11'>
-        <Hamburger />
-        <Header color="black" />
-      </div>
+     
       <div>
         <section className='mt-10 p-2'>
           <section className="header flex mx-auto">
-            <Link className='text-2xl'>
+            <button className='text-2xl' onClick={handleGoBack}>
               &#8592;
-            </Link>
+            </button>
             <Link className='mx-auto fontBold'>
               Foxwrld
             </Link>
           </section>
           <div>
             <section>
-              <div className=''>
-                <div className="shopping-bag uppercase  my-14 text-center fontBold">
+              <div>
+                <div className="shopping-bag uppercase my-14 text-center font-bold">
                   shopping bag (2)
                 </div>
 
                 <div className="shopping-bag__container flex flex-wrap">
-                  <div className="shopping-bag__products md:w-2/4 h-full md:h-[50vh]  overflow-y-scroll">
-                 <CheckoutCard/>
-                 <CheckoutCard/>
-
+                  <div className="shopping-bag__products md:w-2/4 h-full md:h-[50vh] overflow-y-scroll">
+                    <CheckoutCard />
+                    <CheckoutCard />
                   </div>
                   <div className="shopping-bag__payment bg-gray-100 w-full md:w-2/4 p-5 md:h-[30vh] m-3 md:m-0">
                     <h2 className='text-lg font-bold mb-4'>Payment</h2>
@@ -59,7 +61,7 @@ export default function Checkout() {
           </div>
         </section>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
