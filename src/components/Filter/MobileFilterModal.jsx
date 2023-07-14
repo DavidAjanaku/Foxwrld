a107c8c1e66651092552aae04607a9cf5dca5885
DavidAjanaku <davidajanaku46163@gmail.com>
2023-07-14 23:44:34 +0100

    David

--- src/components/Filter/MobileFilterModal.jsx
+++ src/components/Filter/MobileFilterModal.jsx
@@ -59,7 +59,21 @@ const MobileFilterModal = () => {
         onClick={openModal}
         className="bg-black hover:bg-slate-800 text-white font-bold p-5 rounded-full"
       >
-        {/* SVG and button content */}
+       <svg
+  xmlns="http://www.w3.org/2000/svg"
+  className="h-6 w-6"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="currentColor"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth={2}
+    d="M4 6h16M4 12h8m-8 6h16"
+  />
+</svg>
+
       </button>
       <Modal
         isOpen={modalIsOpen}
