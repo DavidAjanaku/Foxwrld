0863acc42e5199307480bdce5eefd6cd107d6465
DavidAjanaku <davidajanaku46163@gmail.com>
2023-07-14 23:39:08 +0100

    David

--- src/components/Filter/MobileFilterModal.jsx
+++ src/components/Filter/MobileFilterModal.jsx
@@ -67,6 +67,25 @@ const MobileFilterModal = () => {
         onRequestClose={closeModal}
         contentLabel="Mobile Filter Modal"
       >
+          <button
+    onClick={closeModal}
+    className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M6 18L18 6M6 6l12 12"
+      />
+    </svg>
+  </button>
         <h2 className="text-2xl font-bold mb-4">Mobile Filter Modal</h2>
         <div className="accordion">
           <button
